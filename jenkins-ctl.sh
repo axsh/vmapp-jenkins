1fbbf7a15d3fbd1929f42f0d8cbd8c73b6530ba5
Manabu Ejima <jemiam@gmail.com>
2013-01-10 21:33:40 +0900

    jenkins-ctl.sh: add 'build' command

--- jenkins-ctl.sh
+++ jenkins-ctl.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-$(dirname BASH_SOURCE[0])/vmbuilder/kvm/rhel/6/misc/kvm-ctl.sh $2 --config_path=$1.conf
+case $2 in
+build)
+  $(dirname BASH_SOURCE[0])/build-vmapp.sh $1
+  ;;
+start|stop)
+  $(dirname BASH_SOURCE[0])/vmbuilder/kvm/rhel/6/misc/kvm-ctl.sh $2 --config_path=$1.conf --viftab=${1}.viftab
+  ;;
+esac
